e19a9697926dac7fddc73dbeeb7a241a0a5a9ecc
HermesWu <1136025519@qq.com>
2018-08-05 11:57:54 +0800

    ADD: g-button-group,并校验其子元素是否为g-button

---
+++ src/button-group.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default{
+        mounted() {
+            let name = this.$el.children
+            for( let node of name) {
+                if(node.nodeName.toLowerCase() !== 'button'){
+                    console.warn(`g-button-group的子元素应该是g-button,但你写的是${node.nodeName}`)
+                }
+            }
+        }
+    }
+</script>
+<style type="text/scss" lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        >.g-button {
+            border-radius: 0;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+            &:hover{
+                position: relative;
+                z-idnex: 1;
+            }
+            &:first-child{
+                border-top-left-radius: var(--button-radius);
+                border-bottom-left-radius: var(--button-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--button-radius);
+                border-bottom-right-radius: var(--button-radius);
+            }
+
+        }
+    }
+</style>
