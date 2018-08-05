a4a6feae60a708000fef9068a82881dc4c9020b5
HermesWu <1136025519@qq.com>
2018-08-05 14:46:16 +0800

    ADD: 安装chai，测试用例

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import chai from 'chai'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+
+const expect = chai.expect
 
 new Vue({
     el:'#app',
