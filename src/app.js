73464df9707d3a12ff2a6e0cbaf8f43d1bf9d689
HermesWu <1136025519@qq.com>
2018-08-05 15:12:20 +0800

    ADD: 使用chai，添加4个测试用例

--- src/app.js
+++ src/app.js
@@ -18,3 +18,65 @@ new Vue({
         loading3: false
     }
 })
+{   // icon
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{   // loading用例
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{   //position 不传order为1
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{   //position 传order为2
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
