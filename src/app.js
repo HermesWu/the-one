f72d9a0b69c04d8a16278765f158fd034052dc5d
吴春祥 <1136025519@qq.com>
2018-08-13 11:34:19 +0800

    ADD:添加input及测试

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 import chai from 'chai'
 import spies from 'chai-spies'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 
 chai.use(spies)
 
---
+++ src/input.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" :disabled="disabled"
+               @change="$emit('change', $event.target.value)"
+               @input="$emit('input', $event.target.value)"
+               @focus="$emit('focus', $event.target.value)"
+               @blur="$emit('blur', $event.target.value)"
+               :readonly="readonly"
+               :value="value">
+        <span class="errorMessage"><svg ><use :xlink:href="'#i-error'"></use></svg>{{error}}</span>
+    </div>
+
+</template>
+<script>
+    export default{
+        props:{
+            value: '',
+            disabled: false,
+            readonly: true,
+            error: '',
+            args:{}
+        }
+    }
+</script>
+<style>
+
+</style>
