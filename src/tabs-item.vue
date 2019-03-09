707d43fa604de919f2270f48bd7e904120d3c8ad
HermesWu <1136025519@qq.com>
2019-03-09 15:51:51 +0800

    禁用 items 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="clicked" :class="classes">
         <g-icon class="center" name="settings"></g-icon>
         <slot></slot>
     </div>
@@ -25,22 +25,20 @@
         },
         mounted(){
             this.eventBus.$on('update:selected', (name, vm)=>{
-                if(name === this.name){
-                    this.active = true
-                }else{
-                    this.active = false
-                }
+                this.active = name === this.name;
             })
         },
         methods: {
-            xxx(){
+            clicked(){
+                if(this.disabled){return}
                 this.eventBus.$emit('update:selected', this.name, this)
             }
         },
         computed:{
             classes(){
                 return{
-                    active: this.active
+                    active: this.active,
+                    disabled: this.disabled
                 }
             }
         }
@@ -58,6 +56,9 @@
         &.active{
             color: $blue;
             font-weight: bold;
+        }
+        &.disabled{
+            color: gray;
         }
         > .center{
             vertical-align:middle;
