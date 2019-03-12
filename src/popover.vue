6bc92e29dacacc1c541433aef1d5063353cadb25
HermesWu <1136025519@qq.com>
2019-03-12 17:30:19 +0800

    添加 popover 默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" v-if="visible" class="content-wrapper">
             <slot name="content" ></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span class="trigger-wrapper" ref="triggerWrapper">
             <slot></slot>
         </span>
     </div>
@@ -18,6 +18,7 @@
         },
         methods:{
             onClickDocument(e){
+                console.log('document关闭');
                 // 防止 content 冒泡到 document 触发关闭div
                 if(!this.$refs.contentWrapper.contains(e.target)){
 
@@ -39,13 +40,13 @@
             },
             close(){
                 this.visible = false
-                console.log('关闭');
                 document.removeEventListener('click',this.onClickDocument)
             },
             onClick(event){
                 // 防止 点击 content 触发 popover 的点击事件
                 if(this.$refs.triggerWrapper.contains(event.target)){
                     if(this.visible === true){
+                        console.log('button关闭');
                         this.close()
                     }else{
                         this.open()
@@ -56,16 +57,45 @@
     }
 </script>
 <style lang="scss" type="text/scss">
+    $border-color: #333;
+    $border-radius: 4px;
     .popover{
         display: inline-block;
         vertical-align: middle;
         position: relative;
+        > .trigger-wrapper{
+            display: inline-block;
+        }
     }
     .content-wrapper{
-        border: 1px solid red;
-        box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        max-width: 20em;
+        padding: 0.5em 1em;
+        /*box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.5);*/
+        filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.5));
+        background-color: white;
         position: absolute;
         left: 0;
         transform: translateY(-100%);
+        margin-top: -10px;
+        &::before, &::after{
+            position: absolute;
+            top: 100%;
+            left: 10px;
+            content: '';
+            display: block;
+            width: 0;
+            height: 0px;
+            border: 10px solid transparent;
+
+        }
+        &::before{
+            border-top-color: black;
+        }
+        &::after{
+            top: calc(100% - 1px);
+            border-top-color: white;
+        }
     }
 </style>
