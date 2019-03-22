f4f3c269954ada04f8a381a9ca7ffdfe041bf8d1
HermesWu <1136025519@qq.com>
2019-03-22 11:39:41 +0800

    删除 .idea/

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="t-cascader">
+    <div class="t-cascader-triger" @click="popoverVisible=!popoverVisible"></div>
+    <div class="t-cascader-popover" v-if="popoverVisible">
+      <cascader-items :items="source" :height="height"></cascader-items>
+    </div>
+  </div>
+</template>
+<script>
+  import CascaderItems from  './cascader-itme'
+
+  export default{
+      name: 'TheOnenCascader',
+      components:{
+        CascaderItems
+      },
+      data(){
+        return {
+            popoverVisible: false,
+        }
+      },
+      props:{
+          source:{
+              type: Array
+          },
+          height:{
+              type: String
+          }
+      },
+      computed:{
+      }
+  }
+</script>
+<style scoped lang="scss">
+  @import '../../styles/_var.scss';
+  .t-cascader{
+    position: relative;
+    .t-cascader-triger{
+      border: 1px solid red;
+      width: 200px;
+      height: 40px;
+
+    }
+    .t-cascader-popover{
+      @extend .box-shadow;
+      display: flex;
+      position: absolute;
+      top:100%;
+      left: 0;
+      background: white;
+    }
+  }
+
+</style>
