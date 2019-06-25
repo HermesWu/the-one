9ddf4eefd7833ee185d8e23f25fbe13e4356274c
HermesWu <1136025519@qq.com>
2019-06-25 14:45:59 +0800

    添加 table 组件

---
+++ src/table/table.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="t-table">
+    <table>
+      <thead>
+      <tr>
+        <th>#</th>
+        <th v-for="column in columns">
+          {{ column.name }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in dataSource">
+        <td>{{ index + 1 }}</td>
+        <template v-for="column in columns">
+          <td>
+            {{ item[column.key] }}
+          </td>
+        </template>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'TTable',
+    data(){
+
+    },
+    props:{
+      columns:{
+        type: Array,
+        required: true
+      },
+      dataSource:{
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+<style type="text/scss" lang="scss">
+
+</style>
