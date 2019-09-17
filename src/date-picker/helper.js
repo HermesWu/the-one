e77783bf0f49e3389295b07c5aa6215af3a46781
HermesWu <1136025519@qq.com>
2019-09-17 15:48:15 +0800

    添加 addMonth addYear

--- src/date-picker/helper.js
+++ src/date-picker/helper.js
@@ -7,6 +7,20 @@ export default {
     let [year, month, day] = getYearMonthDate(date)
     return new Date(year, month + 1, 0)
   },
+  addMonth(date, n){
+    let [_1, month, _2] = getYearMonthDate(date)
+    let newMonth = month + n
+    let copyDate = new Date(date)
+    copyDate.setMonth(newMonth)
+    return copyDate
+  },
+  addYear(date, n){
+    let [year] = getYearMonthDate(date)
+    let newYear = year + n
+    let copyDate = new Date(date)
+    copyDate.setFullYear(newYear)
+    return copyDate
+  },
   getYearMonthDate
 }
 function getYearMonthDate(date){
